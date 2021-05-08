4d50a30b92e700405ec2f939a2a9a85b57a35991
Gabriel Poslanovic <7156@htl.rennweg.at>
2021-05-08 15:04:42 +0200

    Vorbereitung für die Register Tests

---
+++ tests.sql
@@ -0,0 +1,58 @@
+
+/* main.sql zeile 27, sollte noch geändert werden zu not null oder? */
+/* UND in sql steht bei first und lastname z.B. varchar(30), in js aber mit regex {1,}, ändern auf {1,30}? */
+
+/* INSERTS TESTS FÜR REGISTRIERUNG */
+
+# -- Kein Fehler expected -- 
+INSERT INTO user (FirstName, LastName, Username, Email, Passwort, Bio, Insta, Twitter, Soundcloud)
+VALUES ('Gab', 'Posl', 'gabposl', 'gabposl@gmail.com', 'Hallo123', NULL, NULL, NULL, NULL),
+
+# -- Kein Fehler expected -- 
+INSERT INTO user (FirstName, LastName, Username, Email, Passwort, Bio, Insta, Twitter, Soundcloud)
+VALUES ('Gab', 'Posl', 'gabposl', 'gabposl@gmail.com', 'Hallo123', NULL, NULL, NULL, NULL),
+
+# -- Kein Fehler expected -- 
+
+
+# -- Kein Fehler expected -- 
+
+
+# -- Kein Fehler expected -- 
+
+
+# -- Kein Fehler expected -- 
+
+
+# -- Kein Fehler expected -- 
+
+
+# -- Kein Fehler expected -- 
+
+
+# -- Kein Fehler expected -- 
+
+
+# -- Kein Fehler expected -- 
+
+
+# -- Kein Fehler expected -- 
+
+
+# -- Kein Fehler expected -- 
+
+
+# -- Kein Fehler expected -- 
+
+
+# -- Kein Fehler expected -- 
+
+
+# -- Kein Fehler expected -- 
+
+
+# -- Kein Fehler expected -- 
+
+
+# -- Kein Fehler expected -- 
+
